49b28e47c2d1f3b5fc620602eac01c4a54b7ca4a
Nicholas Klein <nicklein@umich.edu>
2021-01-13 14:36:27 -0800

    move more exploratory visualizations for H-feature to new notebook just for looking at this feature, remove clutter in HAS_embedding notebook related to H-feature stuff that was moved out, introduce notebook parameter 'directed' for choosing what kind of graph representation to use in the H-feature computation

--- entity_profiling/h_path_walks.py
+++ entity_profiling/h_path_walks.py
@@ -1,9 +1,14 @@
 import numpy as np
+from kgtk.gt.gt_load import load_graph_from_kgtk
+from kgtk.io.kgtkreader import KgtkReader
+import pathlib
 
+# Performs num_walks random walks at each node in the graph
+# i.e. if there are 10 nodes in the gaph and num_walks=10, then we'll do 10*10 = 100 random walks.
+# Returns a list of walks, where each walk is a list of Q-node strings
+def gt_random_walks(g, walk_length=10, num_walks=10):
+    start_nodes = np.repeat(g.get_vertices(), num_walks)
 
-# Performs a random walk from each start node given
-# Returns a 2d array where each row is a walk made up of vertex indexes
-def random_walk(g, start_nodes, walk_length=10):
     # pre-allocate walks array with initial values of -1
     walks = np.ones((len(start_nodes),walk_length), dtype=int) * -1
     
@@ -22,13 +27,13 @@ def random_walk(g, start_nodes, walk_length=10):
         cur_length += 1
     # trim -1's from any walks that reached a dead end
     walks = [arr[arr >= 0] for arr in walks]
-    return walks
-
-# Performs num_walks random walks at each node in the graph
-# i.e. if there are 10 nodes in the gaph and num_walks=10, then we'll do 10*10 = 100 random walks.
-# Returns a list of walks
-def build_corpus(g, walk_length=10, num_walks=10):
-    start_nodes = np.repeat(g.get_vertices(), num_walks)
-    walks_by_index = random_walk(g, start_nodes)
-    walks_by_qnode = [[g.vp.name[v_ix] for v_ix in walk] for walk in walks_by_index]
+    # change vertex indexes to Q-node names
+    walks_by_qnode = [[g.vp.name[v_ix] for v_ix in walk] for walk in walks]
     return walks_by_qnode
+
+def get_h_walks_from_kgtk_item_file(item_file, directed=False, walk_length=10, num_walks=10):
+    kr = KgtkReader.open(pathlib.Path(item_file))
+    g = load_graph_from_kgtk(kr, directed=directed, hashed=True)
+    h_walks = gt_random_walks(g, walk_length, num_walks)
+    return h_walks
+
