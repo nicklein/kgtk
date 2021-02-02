998ae4603c2caab30eb7348fab953114c8738855
Nicholas Klein <nmklein@usc.edu>
2021-02-02 15:56:33 -0800

    Run notebooks on larger datasets (films and politicians). Various updates to deal with larger sized datasets. Some are just visual, some are changes that need to be made so the notebooks can actually run on these largers datasets. notebook 6 updates currently in-progress

--- entity_profiling/h_path_walks.py
+++ entity_profiling/h_path_walks.py
@@ -4,10 +4,25 @@ from kgtk.io.kgtkreader import KgtkReader
 import pathlib
 
 # Performs num_walks random walks at each node in the graph
-# i.e. if there are 10 nodes in the gaph and num_walks=10, then we'll do 10*10 = 100 random walks.
+# i.e. if there are 10 nodes in the graph and num_walks=10, then we'll do 10*10 = 100 random walks.
+# Appends walks to specified file, where each walk is a list of Q-node strings
+def gt_random_walks_to_file(g, walks_file, walk_length=10, num_walks=10, batch_size=50000):
+    num_batches = int(np.ceil(len(g.get_vertices()) / batch_size))
+    print("num_batches: {}".format(num_batches))
+    for batch_num in range(num_batches):
+        start_nodes = g.get_vertices()[batch_num*batch_size : (batch_num+1)*batch_size]
+        walks = gt_random_walks_from_nodes(g, start_nodes, walk_length, num_walks)
+        # Explicitly cast list of lists to ndarray with dtype=object to avoid ragged nested sequences message
+        walks = np.array(walks, dtype=object)
+        with open(walks_file, "a") as f:
+            np.savetxt(f, walks, fmt="%s")  
+        print("done with batch: {}".format(batch_num + 1))
+
+# Performs num_walks random walks starting from each start node in the input array
+# i.e. if there are 10 nodes in the start node array and num_walks=10, then we'll do 10*10 = 100 random walks.
 # Returns a list of walks, where each walk is a list of Q-node strings
-def gt_random_walks(g, walk_length=10, num_walks=10):
-    start_nodes = np.repeat(g.get_vertices(), num_walks)
+def gt_random_walks_from_nodes(g, start_nodes, walk_length, num_walks):
+    start_nodes = np.repeat(start_nodes, num_walks)
 
     # pre-allocate walks array with initial values of -1
     walks = np.ones((len(start_nodes),walk_length), dtype=int) * -1
@@ -31,9 +46,8 @@ def gt_random_walks(g, walk_length=10, num_walks=10):
     walks_by_qnode = [[g.vp.name[v_ix] for v_ix in walk] for walk in walks]
     return walks_by_qnode
 
-def get_h_walks_from_kgtk_item_file(item_file, directed=False, walk_length=10, num_walks=10):
+def get_h_walks_from_kgtk_item_file(item_file, walks_file, directed=False, walk_length=10, num_walks=10, batch_size=50000):
     kr = KgtkReader.open(pathlib.Path(item_file))
     g = load_graph_from_kgtk(kr, directed=directed, hashed=True)
-    h_walks = gt_random_walks(g, walk_length, num_walks)
-    return h_walks
+    gt_random_walks_to_file(g, walks_file, walk_length, num_walks, batch_size)
 
