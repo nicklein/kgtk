6e7bbb7e47ccdc5e6c92c4537f1dd55f09ce75e1
Nicholas Klein <nmklein@usc.edu>
2021-02-19 17:07:34 -0800

    embedding notebook uses improved walk code and can be simpler now. Cleaned up h_path_walks code a little. Added __init__.py file to make python recognize entity_profiling/ as a module

--- entity_profiling/h_path_walks.py
+++ entity_profiling/h_path_walks.py
@@ -3,21 +3,22 @@ from kgtk.gt.gt_load import load_graph_from_kgtk
 from kgtk.io.kgtkreader import KgtkReader
 import pathlib
 from functools import lru_cache
+from tqdm.auto import tqdm
 
 # Performs num_walks random walks at each node in the graph
 # i.e. if there are 10 nodes in the graph and num_walks=10, then we'll do 10*10 = 100 random walks.
 # Appends walks to specified file, where each walk is a list of Q-node strings
 def gt_random_walks_to_file(g, walks_file, walk_length=10, num_walks=10, batch_size=50000):
-    num_batches = int(np.ceil(len(g.get_vertices()) / batch_size))
+    vertices = g.get_vertices()
+    num_batches = int(np.ceil(len(vertices) / batch_size))
     print("num_batches: {}".format(num_batches))
-    for batch_num in range(num_batches):
-        start_nodes = g.get_vertices()[batch_num*batch_size : (batch_num+1)*batch_size]
+    for batch_num in tqdm(range(num_batches)):
+        start_nodes = vertices[batch_num*batch_size : (batch_num+1)*batch_size]
         walks = gt_random_walks_from_nodes(g, start_nodes, walk_length, num_walks)
         # Explicitly cast list of lists to ndarray with dtype=object to avoid ragged nested sequences message
         walks = np.array(walks, dtype=object)
         with open(walks_file, "a") as f:
-            np.savetxt(f, walks, fmt="%s")  
-        print("done with batch: {}".format(batch_num + 1))
+            np.savetxt(f, walks, fmt="%s")
 
 # Performs num_walks random walks starting from each start node in the input array
 # i.e. if there are 10 nodes in the start node array and num_walks=10, then we'll do 10*10 = 100 random walks.
@@ -30,30 +31,10 @@ def gt_random_walks_from_nodes(g, start_nodes, walk_length, num_walks):
     
     walks[:,0] = start_nodes
     cur_length = 1
-    node_neighs = {}
     while cur_length < walk_length:
         cur_nodes = walks[:,cur_length - 1]
-        # if we've previously hit a dead end, then we could have -1 as a 
-        # current node value in this case, we want to continue filling in -1.
-        next_nodes = list(range(len(cur_nodes)))
-        for i in range(len(cur_nodes)):
-            v = cur_nodes[i]
-            if v < 0:
-                next_nodes[i] = -1
-            else:
-                next_nodes[i] = np.random.choice(gt_get_out_neighbors(g,v))
-#             elif v in node_neighs:
-#                 next_nodes[i] = np.random.choice(node_neighs[v])
-#             else:
-#                 neighs = g.get_out_neighbors(v)
-#                 node_neighs[v] = neighs
-#                 next_nodes[i] = np.random.choice(neighs)      
-
-#         neighbors = [np.array([-1]) if v < 0 else g.get_out_neighbors(v) for v in cur_nodes]
-#         # if there are no outbound edges we can take for 
-#         # some vertex, we'll make the next vertex we visit = -1
-#         neighbors = [np.array([-1]) if len(arr) < 1 else arr for arr in neighbors]
-#         next_nodes = [np.random.choice(arr) for arr in neighbors]
+        # Pad dead ends with -1s
+        next_nodes = [-1 if v < 0 else np.random.choice(gt_get_out_neighbors(g,v)) for v in cur_nodes]
         walks[:,cur_length] = next_nodes
         cur_length += 1
     # trim -1's from any walks that reached a dead end
